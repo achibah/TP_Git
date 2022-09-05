ad96181755f77b7fd71271b55f4477c0fbbfa131
achibah <chibah.aziza@gmail.com>
2022-09-06 00:45:55 +0200

    generation de mon premier monster

---
+++ monstre.py
@@ -0,0 +1,25 @@
+import random
+# création de la classe monstre
+
+class Monster:
+    def __init__(self, pseudo, pv, force, armure):
+        self.pseudo = pseudo
+        self.pv = pv
+        self.force = force
+        self.armure = armure
+
+
+#function
+def create_monster (pseudo, pv, force, armure):
+    monster_object = Monster(pseudo, pv, force, armure)
+    dictionnairy = {
+        "Pseudo": monster_object.pseudo,
+        "PV": monster_object.pv,
+        "Force": monster_object.force,
+        "Armure": monster_object.armure
+    }
+#    return print(personnage.pseudo, personnage.pv, personnage.force, personnage.armure)
+    return print(dictionnairy)
+
+#excecution
+create_monster("Vespoid", random.randint(5,20), random.randint(3,8), random.randint(0,5))
