4deb58ed8a48c90a47be4d8a5d1c36954f91f08b
achibah <chibah.aziza@gmail.com>
2022-09-05 23:01:00 +0200

    creation de la classe

---
+++ personnage.py
@@ -0,0 +1,8 @@
+from tokenize import PseudoExtras
+from typing_extensions import Self
+
+
+class Personnage:
+    def_init_(Self, Pseudo, PV, Force, Armure )
+      self.Pseudo = Pseudo
+      self.PV = 
