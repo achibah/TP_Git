55bf8e57d34e6fa865e2f64cec0d8de3761a5cb2
achibah <chibah.aziza@gmail.com>
2022-09-05 23:34:55 +0200

    creation du personnage

--- personnage.py
+++ personnage.py
@@ -3,6 +3,9 @@ from typing_extensions import Self
 
 
 class Personnage:
-    def_init_(Self, Pseudo, PV, Force, Armure )
-      self.Pseudo = Pseudo
-      self.PV = 
+    def __init__(self, pseudo, pv, force, armure):
+        self.pseudo = pseudo
+        self.pv = pv
+        self.force = force
+        self.armure = armure
+#jksbcjnbc   
